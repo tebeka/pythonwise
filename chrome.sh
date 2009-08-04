f55a71ece64f1bf2be4ec7f47716475b5f949aa3
Miki Tebeka <miki@fattoc.com>
2009-08-03 19:13:47 -0700

    Chrome

---
+++ chrome.sh
@@ -0,0 +1,5 @@
+[17:31] ~ $cd /Applications/Google\ Chrome.app/Contents/MacOS/
+[17:32] MacOS $sudo mv Google\ Chrome _chrome
+[17:32] MacOS $sudo gvim Google\ Chrome
+[17:33] MacOS $chmod +x Google\ Chrome 
+
