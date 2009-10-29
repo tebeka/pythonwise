7e071d86a75435456654b4679c187ac6d1018596
Miki Tebeka <miki.tebeka@gmail.com>
2009-10-28 19:32:42 -0700

    Here we go

---
+++ macapp/humblecalc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Very humble calculator
+
+from __future__ import division
+from math import *
+from operator import isNumberType
+
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from urlparse import urlparse
+from cgi import parse_qs
+import httplib
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        o = urlparse(self.path)
+        if o.path == "/eval":
+            self.eval(o.query)
+        elif o.path == "/quit":
+            self.end_headers()
+            self.wfile.write("Bye")
+            self.wfile.flush()
+            # self.server.shutdown() hangs, so we do it the brutal way
+            import os; os._exit(0)
+        else:
+            SimpleHTTPRequestHandler.do_GET(self)
+
+    def eval(self, query):
+        q = parse_qs(query)
+        expr = q.get("expr", [""])[0]
+        try:
+            # FIXME: Never do this on production, this is a huge security risk
+            result = str(eval(expr))
+        except Exception, e:
+            result = "ERROR"
+
+        self.send_response(httplib.OK)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(result)
+
+if __name__ == "__main__":
+    import webbrowser
+    port = 8822
+    server = HTTPServer(("", port), RequestHandler)
+    webbrowser.open("http://localhost:%s" % port)
+    server.serve_forever()
