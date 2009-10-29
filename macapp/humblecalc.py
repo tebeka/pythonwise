c925eac1131d76b3db1029b8b74ed90d2f8588f1
Miki Tebeka <miki.tebeka@gmail.com>
2009-10-28 20:00:24 -0700

    Comments

--- macapp/humblecalc.py
+++ macapp/humblecalc.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-# Very humble calculator
+# Very humble calculator, written as "Web Desktop Application"
+
+__author__ = "Miki Tebeka <miki.tebeka@gmail.com>"
 
 from __future__ import division
 from math import *
