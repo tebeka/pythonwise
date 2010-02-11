ede2e942a755656305cc417c7cd99c199846d60d
Miki Tebeka <miki.tebeka@gmail.com>
2010-02-10 20:41:49 -0800

    Prase HTTP response

---
+++ httpparse.py
@@ -0,0 +1,25 @@
+# Parse HTTP response
+
+from StringIO import StringIO
+from httplib import HTTPResponse
+
+class FakeSocket(StringIO):
+    def makefile(self, *args, **kw):
+        return self
+
+def httpparse(fp):
+    socket = FakeSocket(fp.read())
+    response = HTTPResponse(socket)
+    response.begin()
+
+    return response
+
+if __name__ == "__main__":
+    from os import popen
+
+    pipe = popen("curl -si http://google.com")
+    response = httpparse(pipe)
+
+    print response.getheaders()
+
+
