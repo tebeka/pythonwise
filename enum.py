bf53533e1d7cfbc0c7f05bcbfcfbcdeb4e902ed5
Miki Tebeka <miki.tebeka@gmail.com>
2011-07-25 13:23:04 -0700

    Docstring and little touches

--- enum.py
+++ enum.py
@@ -2,19 +2,24 @@
 
 def enum(*names, **kw):
     '''
+    Create an enum, names will be ordered starting from 1.
+
+    If you specify `bitmask` keyword, then values will be power of two, useful
+    when you're using a bitmask of flags.
+
     >>> flags = enum("A", "B", "C")
     >>> flags.A
     1
     >>> flags.C
     3
-    >>> flags = enum("A", "B", "C", bitmask=1)
+    >>> flags = enum("A", "B", "C", bitmask=True)
     >>> flags.A
     1
     >>> flags.C
     4
     '''
     class Enum: pass
-    value = (lambda i: 1 << i) if kw.get("bitmask", 0) else (lambda i: i + 1)
+    value = (lambda i: 1 << i) if kw.get('bitmask', 0) else (lambda i: i + 1)
 
     enum = Enum()
     for i, name in enumerate(names):
@@ -22,6 +27,6 @@ def enum(*names, **kw):
 
     return enum
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from doctest import testmod
     testmod(verbose=1)
