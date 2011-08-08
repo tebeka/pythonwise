8c24acd2cead8b7554f8d7bb56d2b0ce7a0c4140
Miki Tebeka <miki.tebeka@gmail.com>
2011-08-07 17:22:44 -0700

    Better tty detection and using argparse

--- calc.py
+++ calc.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 '''Small calculator'''
 
+# ======================================================#
+# Copyright (c) Miki Tebeka <miki.tebeka@gmail.com>     #
+# This file is under the GNU Public License (GPL), see  #
+# http://www.gnu.org/copyleft/gpl.html for more details #
+# ======================================================#
+
 from __future__ import division # Detault to real devision
 from math import * # Get all math goodies in global namespace
 from optparse import OptionParser
 
-USAGE = '''usage: %prog [options] MATH_EXPRESSION
-    e.g. calc '7 * 29' -> 203
-'''
+description = 'Calculate math expression (e.g. calc \'7 * 29\' -> 203)'
 
-def show_gui(text=""):
+def show_gui(text=''):
     import Tkinter as tk
     from operator import isNumberType
 
     root = tk.Tk()
-    root.option_add("*font", "Courier -40 bold")
-    root.bind("<Escape>", lambda e: root.quit())
+    root.option_add('*font', 'Courier -40 bold')
+    root.bind('<Escape>', lambda e: root.quit())
     expr = tk.Entry(root, width=40)
     expr.insert(0, text)
     expr.pack(side=tk.LEFT)
-    tk.Label(root, text="=").pack(side=tk.LEFT)
+    tk.Label(root, text='=').pack(side=tk.LEFT)
     answer = tk.Label(root)
     answer.pack(side=tk.LEFT)
     expr.focus()
@@ -29,11 +33,10 @@ def show_gui(text=""):
         try:
             ans = eval(expr.get())
             if isNumberType(ans):
-                answer["text"] = str(eval(s))
+                answer['text'] = str(eval(s))
         except Exception, e:
-            pass
+            answer['text'] = '???'
         root.after(100, poll)
-
 
     poll()
     root.mainloop()
@@ -43,26 +46,26 @@ def main(argv=None):
         import sys
         argv = sys.argv
 
-    from optparse import OptionParser
+    from argparse import ArgumentParser
 
-    parser = OptionParser(USAGE.strip())
-    parser.add_option("-g", "--gui", help="run in gui mode", 
-        dest="gui", action="store_true", default=0)
 
-    opts, args = parser.parse_args(argv[1:])
+    parser = ArgumentParser(description=description)
+    parser.add_argument('expression', help='math expression', default='',
+                       nargs='?')
+    parser.add_argument('-g', '--gui', help='run in gui mode',
+        dest='gui', action='store_true', default=False)
 
-    nargs = (0, 1) if opts.gui else (1, )
-    if len(args) not in nargs:
-        parser.error("wrong number of arguments") # Will exit
+    args = parser.parse_args(argv[1:])
 
-    if opts.gui:
-        text = args[0] if args else ""
-        show_gui(text)
+    args.gui = (not sys.stdout.isatty()) or args.gui
+
+    if args.gui:
+        show_gui(args.expression)
     else:
         try:
-            print eval(args[0])
-        except Exception, e:
-            raise SystemExit("error: %s" % e)
+            print(eval(args.expression or '0'))
+        except Exception as e:
+            raise SystemExit('error: {0}'.format(e))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
