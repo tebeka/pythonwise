f9f50ece995e89f174aa17183d5a61fbac210ecf
Miki Tebeka <miki.tebeka@gmail.com>
2013-04-08 19:57:03 -0700

    fixing typos

--- sql-col-by-name.py
+++ sql-col-by-name.py
@@ -4,10 +4,10 @@ import sqlite3
 db = sqlite3.connect(":memory:")
 db.row_factory = sqlite3.Row
 
-from psycopy2.extras import DictCursor
-import psycopy2
-db = psycopy2.connect("my-dbn-string")
-cur = db.cursor(row_factory=DictCursor)
+import psycopg2
+from psycopg2.extras import DictCursor
+db = psycopg2.connect("my-dbn-string")
+cur = db.cursor(cursor_factory=DictCursor)
 
 #for row in cur.execute("select * from people"):
 #    print row["name"]
