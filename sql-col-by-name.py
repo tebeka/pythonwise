cf61a2ef08ec52116661b6e5dce3859e492cae4b
Miki Tebeka <miki.tebeka@gmail.com>
2013-04-08 20:02:13 -0700

    formatting

--- sql-col-by-name.py
+++ sql-col-by-name.py
@@ -1,13 +1,17 @@
 '''Get columns by name from SQL query'''
 
+# sqlite3
 import sqlite3
-db = sqlite3.connect(":memory:")
+db = sqlite3.connect(':memory:')
 db.row_factory = sqlite3.Row
 
+# psycopg2
 import psycopg2
 from psycopg2.extras import DictCursor
-db = psycopg2.connect("my-dbn-string")
+db = psycopg2.connect('my-dbn-string')
 cur = db.cursor(cursor_factory=DictCursor)
 
-#for row in cur.execute("select * from people"):
-#    print row["name"]
+# Then
+cur.execute('select * from people')
+for row in cur:
+    print(row['name'])
