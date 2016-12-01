f71811370d572b30ef9841b3a5af75f0263490c5
Miki Tebeka <miki.tebeka@gmail.com>
2016-12-01 16:37:10 +0200

    nicer code

--- pmap.py
+++ pmap.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 '''Parallel map (Unix only)'''
 
-__author__ = "Miki Tebeka <miki@mikitebeka.com>"
-
 # The big advantage of this implementation is that "fork" is very fast on
 # copying data, so if you pass big arrays as arguments and return small values
 # this is a win
 
-# FIXME 
+# FIXME
 # * For too many items, we get "Too many open files"
 # * Handle child exceptions
 
-from os import fork, pipe, fdopen, waitpid, P_WAIT
+from functools import partial
 from marshal import dump, load
-from itertools import takewhile, count
+from os import fork, pipe, fdopen, waitpid, P_WAIT
+
 
 def spawn(func, data):
-    read_fo, write_fo = map(fdopen, pipe(), ("rb", "wb"))
+    read_fo, write_fo = map(fdopen, pipe(), ('rb', 'wb'))
     pid = fork()
-    if pid: # Parent
+    if pid:  # Parent
         return pid, read_fo
 
     # Child
     dump(func(data), write_fo)
     write_fo.close()
     raise SystemExit
+
 
 def wait(child):
     pid, fo = child
@@ -33,15 +33,17 @@ def wait(child):
     fo.close()
     return value
 
+
 def pmap(func, items):
-    '''
+    """Parallel map (using fork) of func over items
     >>> pmap(lambda x: x * 2, range(5))
     [0, 2, 4, 6, 8]
-    '''
-    children = map(lambda item: spawn(func, item), items)
+    """
+    children = map(partial(spawn, func), items)
     return map(wait, children)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     def fib(n):
         a, b = 1, 1
         while n > 1:
@@ -49,7 +51,7 @@ if __name__ == "__main__":
             n -= 1
         return b
 
-    items = range(10)
-    print "pmap(fib, %s)" % str(items)
-    print pmap(fib, items)
-
+    nums = range(10)
+    print('pmap(fib, %s)' % str(nums))
+    for num, val in zip(nums, pmap(fib, nums)):
+        print('%d -> %d' % (num, val))
