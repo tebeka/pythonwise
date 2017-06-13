95abb7e36d1673da1f83950d5deab3d5d587d674
Miki Tebeka <miki.tebeka@gmail.com>
2017-06-13 19:41:34 +0300

    append fixed

--- concat_test.go
+++ concat_test.go
@@ -5,12 +5,15 @@
 $ go test -v -bench . concat_test.go
 === RUN   TestConcatAppend
 --- PASS: TestConcatAppend (0.00s)
+=== RUN   TestConcatAppendFixed
+--- PASS: TestConcatAppendFixed (0.00s)
 === RUN   TestConcatCopy
 --- PASS: TestConcatCopy (0.00s)
-BenchmarkAppend-4   	20000000	       111 ns/op
-BenchmarkCopy-4     	30000000	        46.0 ns/op
+BenchmarkAppend-4        	20000000	       113 ns/op
+BenchmarkAppendFixed-4   	30000000	        48.3 ns/op
+BenchmarkCopy-4          	30000000	        47.8 ns/op
 PASS
-ok  	command-line-arguments	3.774s
+ok  	command-line-arguments	5.373s
 */
 
 package main
@@ -36,12 +39,28 @@ func concatAppend(slices ...[]int) []int {
 	return result
 }
 
-// concatCopy concatinates slices using built-in copy
-func concatCopy(slices ...[]int) []int {
+// calcSize return the total size of all slices
+func calcSize(slices [][]int) int {
 	size := 0
 	for _, slice := range slices {
 		size += len(slice)
 	}
+	return size
+}
+
+// concatAppendFixed concatenates slices using built-in append
+func concatAppendFixed(slices ...[]int) []int {
+	size := calcSize(slices)
+	result := make([]int, 0, size)
+	for _, slice := range slices {
+		result = append(result, slice...)
+	}
+	return result
+}
+
+// concatCopy concatinates slices using built-in copy
+func concatCopy(slices ...[]int) []int {
+	size := calcSize(slices)
 	result := make([]int, size)
 
 	i := 0
@@ -82,6 +101,13 @@ func TestConcatAppend(t *testing.T) {
 	}
 }
 
+func TestConcatAppendFixed(t *testing.T) {
+	out := concatAppend(x, y, z)
+	if !eq(out, res) {
+		t.Fatalf("concatAppendFixed - got %v, expected %v\n", out, res)
+	}
+}
+
 func TestConcatCopy(t *testing.T) {
 	out := concatCopy(x, y, z)
 	if !eq(out, res) {
@@ -92,6 +118,12 @@ func TestConcatCopy(t *testing.T) {
 func BenchmarkAppend(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		concatAppend(x, y, z)
+	}
+}
+
+func BenchmarkAppendFixed(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatAppendFixed(x, y, z)
 	}
 }
 
