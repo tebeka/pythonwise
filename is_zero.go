d3195287e1597d482e252dc198104d59121b0936
Miki Tebeka <miki.tebeka@gmail.com>
2017-09-14 20:31:10 +0300

    check if zero value

---
+++ is_zero.go
@@ -0,0 +1,22 @@
+// Check if a value is the zero value for it's type
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// IsZero returns true if val is the zero value for it's type (e.g. 0, 0.0, "", ...)
+func IsZero(val interface{}) bool {
+	zero := reflect.Zero(reflect.TypeOf(val))
+	return reflect.DeepEqual(zero.Interface(), reflect.ValueOf(val).Interface())
+}
+
+func main() {
+	var m1 map[int]int
+	m2 := map[int]int{1: 2}
+
+	for _, val := range []interface{}{0, 7, "", "hi", m1, m2} {
+		fmt.Printf("%v (%T) -> %v\n", val, val, IsZero(val))
+	}
+}
