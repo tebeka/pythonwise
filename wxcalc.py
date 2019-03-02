2e84e45e47e7ea386ed93d121f3c00bfebe081bf
Miki Tebeka <miki.tebeka@gmail.com>
2019-03-02 17:05:06 +0200

    format

--- wxcalc.py
+++ wxcalc.py
@@ -17,79 +17,81 @@ __version__ = '0.0.3'
 
 import wx
 # So we can evaluate 'sqrt(8)'
-from math import *
+from math import *  # noqa
+
 
 class Calculator(wx.Dialog):
-   '''Main calculator dialog'''
-   def __init__(self):
-       title = 'Calculator version %s' % __version__
-       wx.Dialog.__init__(self, None, -1, title)
-       sizer = wx.BoxSizer(wx.VERTICAL) # Main vertical sizer
+    '''Main calculator dialog'''
+    def __init__(self):
+        title = 'Calculator version %s' % __version__
+        wx.Dialog.__init__(self, None, -1, title)
+        sizer = wx.BoxSizer(wx.VERTICAL)  # Main vertical sizer
 
-       # ____________v
-       self.display = wx.ComboBox(self, -1) # Current calculation
-       sizer.Add(self.display, 0, wx.EXPAND) # Add to main sizer
+        # ____________v
+        self.display = wx.ComboBox(self, -1)  # Current calculation
+        sizer.Add(self.display, 0, wx.EXPAND)  # Add to main sizer
 
-       # [7][8][9][/]
-       # [4][5][6][*]
-       # [1][2][3][-]
-       # [0][.][C][+]
-       gsizer = wx.GridSizer(4, 4)
-       for row in (('7', '8', '9', '/'),
-                   ('4', '5', '6', '*'),
-                   ('1', '2', '3', '-'),
-                   ('0', '.', 'C', '+')):
-           for label in row:
-               b = wx.Button(self, -1, label)
-               gsizer.Add(b)
-               self.Bind(wx.EVT_BUTTON, self.OnButton, b)
-       sizer.Add(gsizer, 1, wx.EXPAND)
+        # [7][8][9][/]
+        # [4][5][6][*]
+        # [1][2][3][-]
+        # [0][.][C][+]
+        gsizer = wx.GridSizer(4, 4)
+        for row in (('7', '8', '9', '/'),
+                    ('4', '5', '6', '*'),
+                    ('1', '2', '3', '-'),
+                    ('0', '.', 'C', '+')):
+            for label in row:
+                b = wx.Button(self, -1, label)
+                gsizer.Add(b)
+                self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(gsizer, 1, wx.EXPAND)
 
-       # [    =     ]
-       b = wx.Button(self, -1, '=')
-       self.Bind(wx.EVT_BUTTON, self.OnButton, b)
-       sizer.Add(b, 0, wx.EXPAND)
-       self.equal = b
+        # [    =     ]
+        b = wx.Button(self, -1, '=')
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b, 0, wx.EXPAND)
+        self.equal = b
 
-       # Set sizer and center
-       self.SetSizer(sizer)
-       sizer.Fit(self)
-       self.CenterOnScreen()
+        # Set sizer and center
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        self.CenterOnScreen()
 
-   def OnButton(self, evt):
-       '''Handle button click event'''
-       # Get title of clicked button
-       label = evt.GetEventObject().GetLabel()
+    def OnButton(self, evt):
+        '''Handle button click event'''
+        # Get title of clicked button
+        label = evt.GetEventObject().GetLabel()
 
-       if label == '=': # Calculate
-           try:
-               compute = self.display.GetValue()
-               # Ignore empty calculation
-               if not compute.strip():
-                   return
+        if label == '=':  # Calculate
+            try:
+                compute = self.display.GetValue()
+                # Ignore empty calculation
+                if not compute.strip():
+                    return
 
-               # Calculate result
-               result = eval(compute)
+                # Calculate result
+                result = eval(compute)
 
-               # Add to history
-               self.display.Insert(compute, 0)
+                # Add to history
+                self.display.Insert(compute, 0)
 
-               # Show result
-               self.display.SetValue(str(result))
-           except Exception as err:
-               wx.LogError(str(err))
-               return
+                # Show result
+                self.display.SetValue(str(result))
+            except Exception as err:
+                wx.LogError(str(err))
+                return
 
-       elif label == 'C': # Clear
-           self.display.SetValue('')
+        elif label == 'C':  # Clear
+            self.display.SetValue('')
 
-       else: # Just add button text to current calculation
-           self.display.SetValue(self.display.GetValue() + label)
-           self.equal.SetFocus() # Set the [=] button in focus
+        else:  # Add button text to current calculation
+            self.display.SetValue(self.display.GetValue() + label)
+            self.equal.SetFocus()  # Set the [=] button in focus
+
 
 if __name__ == '__main__':
-   # Run the application
-   app = wx.App(False)
-   dlg = Calculator()
-   dlg.ShowModal()
-   dlg.Destroy()
+    # Run the application
+    app = wx.App(False)
+    dlg = Calculator()
+    dlg.ShowModal()
+    dlg.Destroy()
