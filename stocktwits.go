548c0625befcfb53a97c2da87a9cbaaf64fe3ba8
Miki Tebeka <miki.tebeka@gmail.com>
2020-01-05 22:50:03 +0200

    comments

--- stocktwits.go
+++ stocktwits.go
@@ -8,19 +8,25 @@ import (
 	"sort"
 )
 
+const (
+	urlTemplate = "https://api.stocktwits.com/api/2/streams/symbol/%s.json"
+)
+
 type count struct {
 	symbol string
 	count  int
 }
 
+// related returns list of related symbols in order
 func related(symbol string) ([]count, error) {
-	url := fmt.Sprintf("https://api.stocktwits.com/api/2/streams/symbol/%s.json", symbol)
+	url := fmt.Sprintf(urlTemplate, symbol)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
+	// Anonymous structure to get only list of symbols per message
 	var reply struct {
 		Messages []struct {
 			Symbols []struct {
@@ -36,14 +42,16 @@ func related(symbol string) ([]count, error) {
 	counts := make(map[string]int)
 	for _, msg := range reply.Messages {
 		for _, sym := range msg.Symbols {
-			counts[sym.Symbol] += 1
+			if sym.Symbol != symbol {
+				counts[sym.Symbol] += 1
+			}
 		}
 	}
 
 	return sorted(counts), nil
-
 }
 
+// sorted retuns sorted list of <symbol, count> (descending)
 func sorted(counts map[string]int) []count {
 	arr := make([]count, 0, len(counts))
 	for sym, n := range counts {
